2d97e17c9cf230b003fc7d8b039a8b68aa78bf48
Diego <96022404+dzfrias@users.noreply.github.com>
2022-11-13 09:16:43 -0800

    feat: add file evaluation

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,67 @@
 use clap::Parser as ArgParser;
 use rustyline::error::ReadlineError;
-use rustyline::{Config, Editor, Result};
+use rustyline::{Config, Editor};
 use scurry;
 use scurry::interpreter::Interpreter;
 use scurry::parser::Parser;
+use std::fs;
+use std::path::PathBuf;
 use std::process;
 
 #[derive(Debug, ArgParser)]
 #[command(author, version, about, long_about = None)]
-struct Args {}
-
-fn main() -> Result<()> {
-    start_repl()
+struct Args {
+    file: Option<PathBuf>,
 }
 
-fn start_repl() -> Result<()> {
+fn main() {
+    let args = Args::parse();
+    match args.file {
+        Some(path) => eval_file(path),
+        None => start_repl(),
+    }
+}
+
+fn eval_file(path: PathBuf) {
+    let contents = match fs::read_to_string(path) {
+        Ok(s) => s,
+        Err(err) => {
+            eprintln!("{err}");
+            process::exit(1);
+        }
+    };
+    let mut interpreter = Interpreter::new();
+    let parser = Parser::new(
+        &contents
+            .strip_suffix('\n')
+            .expect("should have trailing newline in file"),
+    );
+    match parser.parse() {
+        Ok(program) => {
+            if let Err(err) = interpreter.eval(program) {
+                eprintln!("\nruntime error:");
+                eprintln!("{err}");
+                process::exit(1);
+            }
+        }
+        Err(errs) => {
+            println!("\nparser errors:");
+            for err in errs {
+                let s = err.to_string();
+                println!();
+                println!("{}", err.position().format_on_source(&contents));
+                println!("{s}");
+            }
+            process::exit(1);
+        }
+    }
+}
+
+fn start_repl() {
     const PROMPT: &str = ">> ";
 
     let config = Config::builder().indent_size(4).tab_stop(4).build();
-    let mut editor = Editor::<()>::with_config(config)?;
+    let mut editor = Editor::<()>::with_config(config).expect("options should all work");
     let mut interpreter = Interpreter::new();
     loop {
         let readline = editor.readline(PROMPT);
@@ -53,5 +96,4 @@ fn start_repl() -> Result<()> {
             }
         }
     }
-    Ok(())
 }
